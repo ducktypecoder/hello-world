a0fb0cdc0000f6c0fe3b2433a832e42e47f4c093
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-31 06:47:28 -0500

    Adds step

--- ducktypecoder/tests.js
+++ ducktypecoder/tests.js
@@ -1,7 +1,4 @@
-var addNumbers = require('./src/add-numbers');
 
-it('adds numbers', () => {
-  const num1 = 3;
-  const num2 = 4;
-  expect(addNumbers(num1, num2)).toEqual(7);
+it('fails at first', () => {
+  expect(true).toEqual(false);
 });
